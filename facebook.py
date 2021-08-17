64ff4a8a7a592b769153ad71714ec085e4b17456
MarlonAdrian <66731201+MarlonAdrian@users.noreply.github.com>
2021-08-17 18:56:13 -0500

    Update facebook.py

--- facebook.py
+++ facebook.py
@@ -16,7 +16,7 @@ from pymongo.errors import ConnectionFailure
 
 Client = MongoClient('mongodb://localhost:27017')
 
-#Connection check to CouchDB
+#Check connection to CouchDB
 '''try:
     Client.admin.command('ismaster')
     print('MongoDB connection: Succes')
